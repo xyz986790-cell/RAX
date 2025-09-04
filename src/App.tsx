053d8ba23fc1dfa82851c7862618db846b5e5d85
xyz986790-cell <xyz986790@gmail.com>
2025-09-04 18:35:17 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "@/contexts/CartContext";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Philosophy from "./pages/Philosophy";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import BuyNow from "./pages/BuyNow";
+import Product from "./pages/Product";
+import Books from "./pages/Books";
+import Checkout from "./pages/Checkout";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <CartProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/philosophy" element={<Philosophy />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/buy" element={<BuyNow />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/books" element={<Books />} />
+            <Route path="/checkout" element={<Checkout />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
