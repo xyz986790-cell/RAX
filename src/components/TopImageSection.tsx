9445ed22980a609e5b9d30ba5acde485b619c24c
xyz986790-cell <xyz986790@gmail.com>
2026-01-18 13:12:24 +0530

    Create TopImageSection.tsx

---
+++ src/components/TopImageSection.tsx
@@ -0,0 +1,15 @@
+import "./TopImageSection.css";
+
+const TopImageSection = () => {
+  return (
+    <section className="top-image-section">
+      <img
+        src="/assets/top-image.jpg"
+        alt="OMDIC Banner"
+        className="top-image"
+      />
+    </section>
+  );
+};
+
+export default TopImageSection;
